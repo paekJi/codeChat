561c8a0bae792d7a3ec4d9c0faf5886648d55074
DESKTOP-PDOLIU4\admin <skysun@g1ncom.co.kr>
2024-09-25 14:33:29 +0900

    소켓 테스트

--- expressFront/src/chatting/ChatPage.js
+++ expressFront/src/chatting/ChatPage.js
@@ -11,20 +11,42 @@ const ChatPage = ()=> {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
+
+  const[serverMsg, setServerMsg] = useState("");
+
   const location = useLocation();
-  const nickName = location.state.nickName;
+  // const nickName = location.state.nickName!== ? "" :"";
 
   //socket connected 
-  const socket = io("http://localhost:3000", { query: { nickName: nickName } }); 
+  // const socket = io("http://localhost:3000", { query: { nickName: nickName } }); 
+  const socket = io("http://localhost:3000");
+
+  useEffect(()=>{
+
+    //socket connection verify  - not custom, basic event 
+    socket.on("connect", ()=>{
+      console.log(socket.id);
+    });
 
 
+    //sev
+    socket.emit("client-event", "client message");
+    socket.on("server-event", (msg)=>{
+      setServerMsg(msg);
+
+    })
+  
+
+  },[])
+
+  /*
   useEffect(() => {
     socket.on("chat message", (msg) => {
       setMessages((prevMessages) => [...prevMessages, msg]);
     });
 
-    socket.on("user list", (users) => {
-      setUserList(users);
+    socket.on("user list", (usersList) => {
+      setUserList(usersList);
     });
 
 
@@ -33,19 +55,21 @@ const ChatPage = ()=> {
       socket.off("chat message");
       socket.off("user list");
     };
-  }, [socket]);
+  }, []); */
 
   const sendMessage = (e) => {
     e.preventDefault();
     if (message) {
-      socket.emit("chat message", message);
+      // socket.emit("chat message", message);
+      // socket.emit("user list", nickName);
       setMessage("");
     }
   };
 
   return (
     <div>
-      <h1>Chat {nickName}</h1>
+      <h1>Chat</h1>
+      <h2>test :: {serverMsg} </h2>
       <ul>
         {messages.map((msg, index) => (
           <li key={index}>{msg}</li>
--- expressSever/socket.js
+++ expressSever/socket.js
@@ -1,18 +1,26 @@
 module.exports = (io) => {
 
-  var userList = [];
+  io.on("connection", (socket) => {
+
+    socket.on("client-event", (msg)=>{
+      console.log("received client message ==> " + msg);
+      io.emit("server-event", "server message");
+    });
 
 
+
+    
+
+  });
+
+/*
   io.on("connection", (socket) => {
 
     var userNick = socket.handshake.query.nickName;
-    console.log("connected userList")
 
-    if (!userList.includes(userNick)){
-        userList.push(userNick);
-        socket.emit("user list", userList);
-    }
 
+    socket.on("user connect", (userInfo) => {
+    });
 
 
     // 메시지 수신 처리
@@ -20,12 +28,23 @@ module.exports = (io) => {
       io.emit("chat message", msg); // 모든 클라이언트에 메시지 전송
     });
 
+    socket.on("user list", (nickName)=>{
+      if (!userList.includes(userNick)) {
+        userList.push(userNick);
+        socket.emit("user list", userList);
+        console.log("userList list :: ", userList);
+      }
+    })
+
     // 연결 종료 시 이벤트 처리
     socket.on("disconnect", () => {
       console.log("User disconnected");
 
       userList = userList.filter((item) => item !== userNick);
       socket.emit("user list", userList);
-    });
-  });
+    }); 
+
+
+
+  }); */
 };
