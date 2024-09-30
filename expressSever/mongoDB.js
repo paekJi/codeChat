bbc882ebc8f400518dd1574f2a9684ddab223482
paekJi <skysun0803@naver.com>
2024-09-30 23:16:35 +0900

    deprecated option delete

--- expressSever/mongoDB.js
+++ expressSever/mongoDB.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   mongoose
-    .connect("mongodb://localhost:27017/test", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect("mongodb://localhost:27017/test")
     .then(() => {
       console.log("mongoDB is connected");
     });
