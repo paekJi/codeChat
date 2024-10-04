be3fb95c15fee87039c7dd572491f39fac230a5f
DESKTOP-PDOLIU4\admin <skysun@g1ncom.co.kr>
2024-10-04 16:35:10 +0900

    sqllite 세팅

--- expressSever/app.js
+++ expressSever/app.js
@@ -8,22 +8,21 @@ const socketIo = require("socket.io");
 const path = require("path");
 const app = express();
 const PORT = 3000;
-require("./mongoDB")();
 const cookieParser = require("cookie-parser");
 
 
 const server = http.createServer(app);
 const io = socketIo(server);
 
-require("./socket/socket.js")(io);
-
 
 const appConfig = require("./config/config.js")
 const loginRouter = require("./router/loginRoute.js");
 const chatRouter = require("./router/chatRoute.js");
 
-
-/**chat log saver */
+/** database / socket connect  */
+require("./socket/socket.js")(io);
+require("./db/mongoDB")();
+require("./db/sqllite.js")(appConfig.sqlliteLocation);
 
 /** import end */
 app.use(cors());
@@ -40,11 +39,9 @@ app.use("/api/chat",chatRouter);
 app.use(express.static(path.join(__dirname, "../expressFront/public")));
 
 app.get("*",(req, res)=>{
- res.sendFile(path.join(__dirname, "../expressFront/public/index.html"));
+  res.sendFile(path.join(__dirname, "../expressFront/public/index.html"));
 })
 
-
-
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT} port`);
 });
